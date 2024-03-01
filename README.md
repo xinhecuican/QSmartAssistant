d6b46746bd79c24793930a3dcdb1a32de5b74255
xinhecuican <lizilin2686462757@gmail.com>
2024-03-01 11:40:10 +0800

    append readme

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
+# lowpower robot
+
+一个模块化，全过程可离线，低占用率，高效率的对话机器人/智能音箱
+
+# 特点
+
+- [x] 使用c++qt开发，深度学习模型大多选择onnxruntime，提高运行速度
+- [x] 前处理，唤醒，语音端点检测，ASR，TTS，NLU全模块化，可以非常简单的开发组件。拥有插件机制，可以定制自己的插件
+- [x] 支持porcupine,openwakeword,duilite三大唤醒引擎，可以自由定制唤醒词。支持cobra,fvad,silero,duilite四大vad引擎。搭配webrtc和speex，可以精确唤醒并降低误唤醒率。
+- [x] 支持流式ASR和TTS
+- [x] 支持rasa，可以自己收集对话语料训练NLU
+- [x] 支持自定义插件，可以选择加载哪些插件及加载顺序
+- [ ] 支持api
+- [ ] 支持多机器联合控制
+
+# 处理流程
+
+![lowpower_robot流程](https://image.xinhecuican.tech/img/lowpower_robot%E6%B5%81%E7%A8%8B.png)
+
+麦克风输入的语音信号经过前处理处理掉噪音，混响和回声后，送入唤醒引擎。如果成功唤醒则激活Vad来收集说话人的命令。再将语音信号提交给ASR转换成文字。NLU根据文本获得其中的意图，然后交由插件系统处理，最后使用TTS将处理结果说出来。
+
 # 安装
 
-qt5.15.3
-需要安装multimedia相关的库
+见[安装教程](https://github.com/xinhecuican/lowpower-robot/wiki/%E5%AE%89%E8%A3%85)
--- scripts/install_rasa.sh
+++ scripts/install_rasa.sh
@@ -1,17 +1,19 @@
+#!/bin/bash
 current_path=$(cd $(dirname $0); pwd)
 parent_path=$(dirname ${current_path})
 lib_path=${parent_path}/lib/rasa
 bert_path=${parent_path}/Data/rasa/data/bert-base-chinese
 
 python -m venv ${lib_path}
+echo ${lib_path}/bin/activate
 source ${lib_path}/bin/activate
 pip install rasa
 pip install jieba
 pip install transformers
 mkdir -p ${bert_path}
 cd ${bert_path}
-wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/config.json?download=true
-wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/pytorch_model.bin?download=true
-wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/tf_model.h5?download=true
-wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/tokenizer_config.json?download=true
-wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/vocab.txt?download=true
+wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/config.json
+wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/pytorch_model.bin
+wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/tf_model.h5
+wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/tokenizer_config.json
+wget https://huggingface.co/google-bert/bert-base-chinese/resolve/main/vocab.txt
