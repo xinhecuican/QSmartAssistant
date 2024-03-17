41d5f4f81003cea610f1c604bdf84998e8208da7
xinhecuican <lizilin2686462757@gmail.com>
2024-03-17 12:22:57 +0800

    update readme

--- Plugins/Plugin.h
+++ Plugins/Plugin.h
@@ -4,25 +4,61 @@
 #include "IPluginHelper.h"
 #include <QObject>
 
-struct PluginMessage{
+struct PluginMessage {
     QString src;
     QString dst;
     QString message;
 };
 Q_DECLARE_METATYPE(PluginMessage)
 
-class Plugin{
+class Plugin {
 public:
-    virtual ~Plugin(){}
-    virtual QString getName()=0;
-    virtual void setPluginHelper(IPluginHelper* helper)=0;
-    virtual bool handle(const QString& text,
-                        const ParsedIntent& parsedIntent,
-                        bool& isImmersive)=0;
-    virtual void recvMessage(const PluginMessage& message)=0;
+    virtual ~Plugin() {}
+    /**
+     * @brief Get Plugin Name
+     * Name is equal to project name
+     *
+     * @return QString
+     */
+    virtual QString getName() = 0;
 
-// signals:
-    virtual void sendMessage(PluginMessage message)=0;
+    /**
+     * @brief set IPluginHelper
+     * recommand init in this func
+     *
+     * @param helper pointer of IPluginHelper object
+     */
+    virtual void setPluginHelper(IPluginHelper *helper) = 0;
+
+    /**
+     * @brief handle intent from nlu
+     * @p isImmersive set to true means
+     * next intent will handle by this plugins first
+     *
+     * @param text original text from asr
+     * @param parsedIntent parsed intent from nlu
+     * @param isImmersive in/out try to set immersive mode
+     * @return true successfully handle, stop pass to another plugin
+     * @return false don't handle the intent
+     */
+    virtual bool handle(const QString &text, const ParsedIntent &parsedIntent,
+                        bool &isImmersive) = 0;
+
+    /**
+     * @brief receive message from another plugin
+     *
+     * @param message
+     */
+    virtual void recvMessage(const PluginMessage &message) = 0;
+
+    // signals:
+    /**
+     * @brief send message to another plugin
+     * if message.message ="handle" means trigger handle of other plugin
+     *
+     * @param message
+     */
+    virtual void sendMessage(PluginMessage message) = 0;
 };
 #define LOWPOWER_ROBOT_PLUGIN_ID "lowpwoer_robot_plugin1.0"
 Q_DECLARE_INTERFACE(Plugin, LOWPOWER_ROBOT_PLUGIN_ID)
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 - [x] 使用c++qt开发，深度学习模型大多选择onnxruntime，提高运行速度
 - [x] 前处理，唤醒，语音端点检测，ASR，TTS，NLU全模块化，可以非常简单的开发组件。拥有插件机制，可以定制自己的插件
-- [x] 支持porcupine,openwakeword,duilite三大唤醒引擎，可以自由定制唤醒词。支持cobra,fvad,silero,duilite四大vad引擎。搭配webrtc和speex，可以精确唤醒并降低误唤醒率。
+- [x] 支持porcupine,openwakeword,duilite,snowboy唤醒模型，可以自由定制唤醒词。支持cobra,fvad,silero,duilitevad模型。搭配webrtc和speex，可以精确唤醒并降低误唤醒率。
 - [x] 支持流式ASR和TTS
 - [x] 支持rasa，可以自己收集对话语料训练NLU
 - [x] 支持自定义插件，可以选择加载哪些插件及加载顺序
@@ -19,6 +19,17 @@
 
 麦克风输入的语音信号经过前处理处理掉噪音，混响和回声后，送入唤醒引擎。如果成功唤醒则激活Vad来收集说话人的命令。再将语音信号提交给ASR转换成文字。NLU根据文本获得其中的意图，然后交由插件系统处理，最后使用TTS将处理结果说出来。
 
-# 安装
+# 硬件环境
 
-见[安装教程](https://github.com/xinhecuican/lowpower-robot/wiki/%E5%AE%89%E8%A3%85)
+测试机器：
+
+- arm 树莓派4b 64位
+- x86 ubuntu22.04
+
+内存需求：全过程离线的情况下占用大约在1.2G到1.4G之间，推荐至少2G内存，并且需要开2G的虚拟内存
+
+# 安装和配置
+
+见[安装教程](https://github.com/xinhecuican/lowpower-robot/wiki/%E5%AE%89%E8%A3%85)和[配置教程](https://github.com/xinhecuican/lowpower-robot/wiki/%E9%85%8D%E7%BD%AE)
+
+[插件配置详见](https://github.com/xinhecuican/lowpower-robot/wiki/%E6%8F%92%E4%BB%B6)
